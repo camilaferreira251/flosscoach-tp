9f0394c627dff80bd5b9d7aa42d4f546385137ba
Camila Ferreira <camilaferreira251@gmail.com>
2016-08-28 14:14:15 -0300

    omni_auth_login_controler coments

--- app/controllers/omni_auth_login_controller.rb
+++ app/controllers/omni_auth_login_controller.rb
@@ -1,9 +1,14 @@
+# Controler of login with omni auth.
+
 class OmniAuthLoginController < ApplicationController
 	def index
 		if current_user
 			redirect_to projects_path
 		end
   	end
+
+  	# Create a new login with omni auth.
+
 	def create
 	    auth = request.env["omniauth.auth"]
 	    user = User.find_or_create_with_omniauth(auth)
@@ -11,9 +16,13 @@ class OmniAuthLoginController < ApplicationController
 		redirect_to projects_path
 	end
 
+	# Redrect login when failure
+
 	def failure
 		redirect_to root_url
 	end
+
+	#Destroy login with omni auth and redrect for index.
 
 	def destroy
 		session.delete(:user_id)
