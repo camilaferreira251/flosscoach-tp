f74d63ad9592df50c9ce6f33be6c40a639730e8b
Matheus Bernardo <matheussbernardo@gmail.com>
2016-08-30 22:03:18 -0300

    fixing identation inconsistency

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -49,12 +49,12 @@ class LanguagesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_language
-      @language = Language.find(params[:id])
-    end
+  def set_language
+    @language = Language.find(params[:id])
+  end
 
     # Only allow a trusted parameter "white list" through.
-    def language_params
-      params.require(:language).permit(:name)
-    end
+  def language_params
+    params.require(:language).permit(:name)
+  end
 end
