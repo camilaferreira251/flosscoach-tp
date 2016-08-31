afefa5c9217753757e2bc24cdf631d9a0f1ba471
Matheus Bernardo <matheussbernardo@gmail.com>
2016-08-30 22:31:30 -0300

    fixing inconsistent identation

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,43 +2,40 @@
 
 # Redirect user after login.
 class LoginController < ApplicationController
-	def index
-		if current_user
-			redirect_to projects_path
-		else
-			#nothing to do.
-		end
-	end
+  def index
+    if current_user
+      redirect_to projects_path
+    else
+      # nothing to do.
+    end
+  end
 
-	# Create new user login.
-	def create
-		user = User.find_by_email(params[:user][:email])
+  # Create new user login.
+  def create
+    user = User.find_by_email(params[:user][:email])
+    if user && user.valid_password?(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to projects_path
+      # redirect_to @user
+    else
+      flash.now[:alert] = "Invalid e-mail or password."
+      render action: "index"
+    end
+  end
 
-		if user && user.valid_password?(params[:user][:password])
-			session[:user_id] = user.id
-			redirect_to projects_path
-			#redirect_to @user
-		else
-			flash.now[:alert] = "Invalid e-mail or password."
-			render action: "index"
-		end
-	end
+  # User logout.
+  def logout
+    session.delete(:user_id)
+    render action: "index"
+  end
 
-	# User logout.
-
-	def logout
-		session.delete(:user_id)
-		render action: "index"
-	end
-
-  	# Search of projects for the user.
-
-	def search
-		@projects = Project.search(params[:query])
-		if request.xhr?
-			render :json => @projects.to_json
-		else
-			render :index
-		end
-	end
+  # Search of projects for the user.
+  def search
+    @projects = Project.search(params[:query])
+    if request.xhr?
+      render :json => @projects.to_json
+    else
+      render :index
+    end
+  end
 end
--- app/controllers/omni_auth_login_controller.rb
+++ app/controllers/omni_auth_login_controller.rb
@@ -1,33 +1,33 @@
 # Controler of login with omni auth.
 
 class OmniAuthLoginController < ApplicationController
-	def index
-		if current_user
-			redirect_to projects_path
-		else
-			#nothing to do.
-		end
-	end
+  def index
+    if current_user
+      redirect_to projects_path
+    else
+      #nothing to do.
+    end
+  end
 
-  	# Create a new login with omni auth.
+  # Create a new login with omni auth.
 
-	def create
-	    auth = request.env["omniauth.auth"]
-	    user = User.find_or_create_with_omniauth(auth)
-	    session[:user_id] = user.id
-		redirect_to projects_path
-	end
+  def create
+    auth = request.env["omniauth.auth"]
+    user = User.find_or_create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to projects_path
+  end
 
-	# Redrect login when failure
+  # Redrect login when failure
 
-	def failure
-		redirect_to root_url
-	end
+  def failure
+    redirect_to root_url
+  end
 
-	#Destroy login with omni auth and redrect for index.
+  # Destroy login with omni auth and redrect for index.
 
-	def destroy
-		session.delete(:user_id)
-		render action: "index"
-	end
+  def destroy
+    session.delete(:user_id)
+    render action: "index"
+  end
 end
