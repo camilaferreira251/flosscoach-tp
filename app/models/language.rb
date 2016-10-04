3ebd52abdb0cd96372be772e9fc4e2366d3d8510
Camila Ferreira <camilaferreira251@gmail.com>
2016-10-04 10:55:50 -0300

    include header in languages.rb

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,9 @@
+########################################
+# File:languages.rb
+# Porpose: Indicates relationships with languages
+# GNU AGPLv3
+########################################
+
 class Language < ActiveRecord::Base
 	has_many :projects
 end
