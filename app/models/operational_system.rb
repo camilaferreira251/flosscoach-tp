e0c684ec9ba41bfa4f2b92526cadd2d13ac93c67
Camila Ferreira <camilaferreira251@gmail.com>
2016-10-04 10:57:14 -0300

    include header in operational_system.rb

--- app/models/operational_system.rb
+++ app/models/operational_system.rb
@@ -1,3 +1,9 @@
+########################################
+# File:operational_systems.rb
+# Porpose: Indicates relationships with operational systems
+# GNU AGPLv3
+########################################
+
 class OperationalSystem < ActiveRecord::Base
 	has_many :projects
 end
