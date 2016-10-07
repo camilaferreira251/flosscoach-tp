bade814b7eecc702b89f23afd9d41565d93546f8
Camila Ferreira <camilaferreira251@gmail.com>
2016-10-07 10:55:11 -0300

    comment class open_hub_project_collection

--- app/models/open_hub_project_collection.rb
+++ app/models/open_hub_project_collection.rb
@@ -1,3 +1,5 @@
+# Class for initialize openhub API for other classes can use your data.
+
 class OpenHubProjectCollection < ActiveResource::Collection
   def initialize(parsed = {})
     @elements = parsed['result']
