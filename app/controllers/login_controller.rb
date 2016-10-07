b26adc09bcb761f89bc68e3aee83889496d728c4
Matheus Bernardo <matheussbernardo@gmail.com>
2016-10-07 16:17:58 -0300

    inserting assertions

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -18,6 +18,7 @@ class LoginController < ApplicationController
     # Crete a new login user with email valid params or alert user that params
     # are invalids. 
     user = User.find_by_email(params[:user][:email])
+    assert(user.kind_of?(User))
     if user && user.valid_password?(params[:user][:password]) # check if user exists and if is a valid passoword
       session[:user_id] = user.id
       redirect_to projects_path
@@ -30,12 +31,14 @@ class LoginController < ApplicationController
   # User logout.
   def logout
     session.delete(:user_id)
+    assert(session[:user_id] == nil)
     render action: 'index'
   end
 
   # Search of projects for the user.
   def search
     @projects = Project.search(params[:query])
+    assert(@projects.first.kind_of(Project))
     # Search params of project that user puts or in case that not exist
     # put user in idex page.
     if request.xhr? # check if request is ajax
