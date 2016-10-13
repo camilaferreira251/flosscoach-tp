6660ea70af58458a7e2035f65a4700c1000ac2ce
Camila Ferreira <camilaferreira251@gmail.com>
2016-10-13 10:03:14 -0300

    comment class open_hub_project

--- app/models/open_hub_project.rb
+++ app/models/open_hub_project.rb
@@ -1,20 +1,26 @@
 # Class with structure to use data of openhug API
 
 require 'active_resource'
-#this class is a XML formater that uses active resources
+# This class is a XML formater that uses active resources
 class MyXMLFormatter
   include ActiveResource::Formats::XmlFormat
 
+  # Find the project in the openhub XML.
   def decode(xml)
     ActiveResource::Formats::XmlFormat.decode(xml)['result']['project']
   end
 end
 
-# this class defines an open hub project
+# This class defines an open hub project
 class OpenHubProject < ActiveResource::Base
 
+  # Openhub site
   self.site = "https://www.openhub.net/"
+
+  # Define format of openhub XML
   self.format =  MyXMLFormatter.new
+
+  # Define the name os element for serch
   self.element_name = "projects"
   
   @headers = { 'api_key' => ENV["OPENHUB_KEY"] }
@@ -22,6 +28,8 @@ class OpenHubProject < ActiveResource::Base
   def self.find_by_id(id)
     self.find(id, :params => {'api_key' => ENV["OPENHUB_KEY"]})
   end
+
+  #Define params fo search on openhub.
   def self.find_by_name(nome)
     self.find(:all, :params => {'query' => nome ,'api_key' => ENV["OPENHUB_KEY"]})
   end
